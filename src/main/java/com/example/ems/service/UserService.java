a0a1850d3580b0d52cba09115529866fab7db990
prince205927 <sapkotaprince17@gmail.com>
2025-11-05 21:59:44 +0545

    feat:login feature implemented

--- src/main/java/com/example/ems/service/UserService.java
+++ src/main/java/com/example/ems/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.ems.service;
 
+import com.example.ems.dto.LoginRequest;
+import com.example.ems.dto.LoginResponse;
 import com.example.ems.dto.RegisterRequest;
 import com.example.ems.entity.User;
 import com.example.ems.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +32,13 @@ public class UserService {
         userRepository.save(user);
         
         return "User registered successfully!";
+    }
+    
+    public LoginResponse loginUser(LoginRequest loginRequest) {
+    	User user = userRepository.findByUsername(loginRequest.getUsername()).orElseThrow(() -> new BadCredentialsException("Invalid username or password"));
+    	if(!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+    		throw new BadCredentialsException("Invalid username or password");
+    	}
+    	return new LoginResponse(user.getId(), user.getUsername(), user.getRole(), "Login successful");
     }
 }
